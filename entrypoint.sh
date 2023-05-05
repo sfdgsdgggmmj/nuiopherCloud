b7afebb716f3fd1b0005e49fc1b5b2c1d6465936
sfdgsdgggmmj <131497970+sfdgsdgggmmj@users.noreply.github.com>
2023-05-05 15:38:18 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='56ddaaae-71f0-408b-9618-8d57d7c139dc'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
